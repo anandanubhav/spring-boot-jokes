2e910b467ab30a67f4c47ea2143f91d8931884d7
Anubhav Anand <aa7784@gmail.com>
2018-12-19 23:04:54 +0530

    added xml based configuration for bean

--- src/main/java/com/aa/jokes/SpringBootJokesApplication.java
+++ src/main/java/com/aa/jokes/SpringBootJokesApplication.java
@@ -2,8 +2,10 @@ package com.aa.jokes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:jokes-config.xml")
 public class SpringBootJokesApplication {
 
     public static void main(String[] args) {
